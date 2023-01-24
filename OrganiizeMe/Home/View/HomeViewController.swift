e113a247ac83ed602fd30de7fbdad5963b9a7a5c
Bianca Ferreira <laferreira.bianca@gmail.com>
2023-01-24 17:41:29 -0300

    fix: home view controller

--- OrganiizeMe/Home/View/HomeViewController.swift
+++ OrganiizeMe/Home/View/HomeViewController.swift
@@ -13,7 +13,15 @@ class HomeViewController: UIViewController {
     var coordinator: HomeBaseCoordinator?
     
     //MARK: - Views
-    let profileView = ProfileView()
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(frame: .zero)
+        stack.axis = .vertical
+        stack.spacing = 10.0
+        stack.alignment = .fill
+        stack.distribution = .fillProportionally
+        
+        return stackView
+    }()
     
     //MARK: - Life Cycle
     init(coordinator: HomeBaseCoordinator) {
@@ -29,52 +37,43 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(profileView)
+        setupViews()
         setupConstraints()
     }
     
     //MARK: - Functions
+    func setupViews() {
+        view.addSubview(stackView)
+    }
+    
     func setupConstraints() {
-        profileView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            profileView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            profileView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            profileView.heightAnchor.constraint(equalToConstant: 50.0)
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            stackView.heightAnchor.constraint(equalToConstant: 50.0)
         ])
     }
 }
 
-class ProfileView: UIView {
-    
-    //MARK: - Lazy Variables
-    lazy var stackView: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.spacing = 10.0
-        stack.alignment = .fill
-        stack.distribution = .fillProportionally
-        return stackView
-    }()
-    
-    //MARK: - Life Cycle
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .purple
+#if DEBUG
+import SwiftUI
+
+struct HomeViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        lazy var homeCoordinator: HomeBaseCoordinator = HomeCoordinator()
+        return HomeViewController(coordinator: homeCoordinator)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    //MARK: - Functions
-    func setupView() {
-        
-    }
-    
-    func addSubviews() {
-        
-    }
-    
-    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
 }
+
+struct HomeViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        HomeViewControllerRepresentable()
+            .previewDevice("iPhone SE (3rd generation)")
+    }
+}
+
+#endif
