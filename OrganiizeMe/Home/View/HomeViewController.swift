081f2ed9147d5ae737a7510b14b0a5cc9e57fb82
Bianca Ferreira <laferreira.bianca@gmail.com>
2023-01-24 16:51:23 -0300

    feat: adding homeviewcontroller interface

--- OrganiizeMe/Home/View/HomeViewController.swift
+++ OrganiizeMe/Home/View/HomeViewController.swift
@@ -12,6 +12,10 @@ class HomeViewController: UIViewController {
     //MARK: - Variables
     var coordinator: HomeBaseCoordinator?
     
+    //MARK: - Views
+    let profileView = ProfileView()
+    
+    //MARK: - Life Cycle
     init(coordinator: HomeBaseCoordinator) {
         super.init(nibName: nil, bundle: nil)
         self.coordinator = coordinator
@@ -21,11 +25,56 @@ class HomeViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(profileView)
+        setupConstraints()
     }
-
-
+    
+    //MARK: - Functions
+    func setupConstraints() {
+        profileView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            profileView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            profileView.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+    }
 }
 
+class ProfileView: UIView {
+    
+    //MARK: - Lazy Variables
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10.0
+        stack.alignment = .fill
+        stack.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    //MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Functions
+    func setupView() {
+        
+    }
+    
+    func addSubviews() {
+        
+    }
+    
+    
+}
