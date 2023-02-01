a40ed941ae7d5db2fffa26cdad0e51a516dfc2ed
Bianca Ferreira <laferreira.bianca@gmail.com>
2023-02-01 12:53:04 -0300

    fix: additem stack view layout and add new task viewcontroller layout

--- OrganiizeMe/AddTask/SubViews/AddItemStackView.swift
+++ OrganiizeMe/AddTask/SubViews/AddItemStackView.swift
@@ -1,5 +1,5 @@
 //
-//  AddTaskStackView.swift
+//  AddItemStackView.swift
 //  OrganiizeMe
 //
 //  Created by Bianca Ferreira on 31/01/23.
@@ -7,37 +7,48 @@
 
 import UIKit
 
-class AddTaskStackView: UIStackView {
+class AddItemStackView: UIStackView {
     
     //MARK: - Views
     lazy var taskNameTextField: UITextField = {
-        let textField = FloatingTextField(frame: .zero)
+        let textField = UITextField(frame: .zero)
         textField.placeholder = "TaskNamePlaceholder".localized
         textField.textColor = .black
-        textField.contentMode = .scaleToFill
+        textField.contentMode = .scaleAspectFit
         textField.clipsToBounds = true
         textField.font = UIFont.italicSystemFont(ofSize: 16)
         textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
     lazy var taskDescriptionTextField: UITextField = {
-        let textField = FloatingTextField(frame: .zero)
+        let textField = UITextField(frame: .zero)
         textField.placeholder = "Descrição"
         textField.textColor = .black
-        textField.contentMode = .scaleToFill
+        textField.contentMode = .scaleAspectFit
         textField.clipsToBounds = true
         textField.font = UIFont.italicSystemFont(ofSize: 16)
         textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
+    //TODO: Put the date picker with label
     lazy var taskIntialDate: UIDatePicker = {
         let datePicker = UIDatePicker(frame: .zero)
         datePicker.timeZone = NSTimeZone.local
-        datePicker.backgroundColor = .white
+        datePicker.datePickerMode = .date
+        
+        return datePicker
+    }()
+    
+    lazy var taskFinalDate: UIDatePicker = {
+        let datePicker = UIDatePicker(frame: .zero)
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.datePickerMode = .date
         return datePicker
     }()
     
@@ -52,21 +63,32 @@ class AddTaskStackView: UIStackView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Functions
-    func setupStackView() {
+    //MARK: - Private Functions
+    private func setupStackView() {
         self.axis = .vertical
         self.alignment = .center
-        self.distribution = .fillProportionally
-        self.backgroundColor = .purple
+        self.distribution = .fill
+        self.backgroundColor = .clear
         self.preservesSuperviewLayoutMargins = true
         self.layer.cornerRadius = 10
-        self.spacing = 10
+        self.spacing = 20
+        self.backgroundColor = #colorLiteral(red: 0.1529411765, green: 0.3490196078, blue: 0.3137254902, alpha: 1).withAlphaComponent(0.1)
         self.layoutSubviews()
     }
     
-    func setupConstraints() {
+    private func setupConstraints() {
         self.addArrangedSubview(taskNameTextField)
         self.addArrangedSubview(taskDescriptionTextField)
         self.addArrangedSubview(taskIntialDate)
+        self.addArrangedSubview(taskFinalDate)
+        
+        NSLayoutConstraint.activate([
+            taskDescriptionTextField.heightAnchor.constraint(equalToConstant: 100),
+        ])
+        
+        NSLayoutConstraint.activate([
+            taskNameTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            taskDescriptionTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+        ])
     }
 }
--- OrganiizeMe/AddTask/View/AddNewTaskViewController.swift
+++ OrganiizeMe/AddTask/View/AddNewTaskViewController.swift
@@ -10,28 +10,73 @@ import UIKit
 class AddNewTaskViewController: UIViewController {
     
     //MARK: - Views
-    var stackView = AddTaskStackView(frame: .zero)
+    var stackView = AddItemStackView(frame: .zero)
+    lazy var addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Adicionar", for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.shadowColor = #colorLiteral(red: 0.1529411765, green: 0.3490196078, blue: 0.3137254902, alpha: 1)
+        button.layer.shadowOpacity = 1
+        button.layer.shadowOffset = CGSize(width: 1.5, height: 4)
+        button.layer.shadowRadius = 0.5
+        button.layer.masksToBounds = false
+        button.backgroundColor = #colorLiteral(red: 0.2549019608, green: 0.7490196078, blue: 0.7019607843, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .darkGray
+        view.backgroundColor = #colorLiteral(red: 0.5529411765, green: 0.9490196078, blue: 0.9098039216, alpha: 1)
+        title = "Adicionar Tarefa"
         setupViews()
         setupConstraints()
     }
     
+    //MARK: - Functions
     func setupViews() {
+        //MARK: Setup StackView
         view.addSubview(stackView)
+        view.addSubview(addButton)
+        stackView.layer.cornerRadius = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    //MARK: - Private Functions
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            stackView.heightAnchor.constraint(equalToConstant: 200)
+//            stackView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30),
+            addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addButton.heightAnchor.constraint(equalToConstant: 40),
+            addButton.widthAnchor.constraint(equalToConstant: 200),
         ])
     }
-    
 }
+
+#if DEBUG
+import SwiftUI
+
+struct AddNewTaskViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return AddNewTaskViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
+
+struct AddNewTaskViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        AddNewTaskViewControllerRepresentable()
+            .previewDevice("iPhone SE (3rd generation)")
+    }
+}
+
+#endif
